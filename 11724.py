22be3d5cfaff96d6e245a5067cf5530abdd3cef2
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-07-24 18:06:16 +0900

    Create 11724.py

---
+++ 11724.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+N, M = map(int, input().split())
+adj = [[0] * N for _ in range(N)]
+for _ in range(M):
+    a, b = map(lambda x: x - 1, map(int, input().split()))
+    adj[a][b] = adj[b][a] = 1
+
+ans = 0
+chk = [False] * N
+
+
+def dfs(now):
+    for nxt in range(N):
+        if adj[now][nxt] and not chk[nxt]:
+            chk[nxt] = True
+            dfs(nxt)
+
+
+for i in range(N):
+    if not chk[i]:
+        ans += 1
+        chk[i] = True
+        dfs(i)
+
+print(ans)
