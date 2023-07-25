fa629e2426673791b29844d10fca415e8d1fe02c
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-07-26 07:30:47 +0900

    Create 11051.py

---
+++ 11051.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit(10 ** 7)
+
+cache = [[0] * 1001 for _ in range(1001)]
+
+
+MOD = 10007
+
+N, K = map(int, input().split())
+
+def bino(n, k):
+    if cache[n][k]:
+        return cache[n][k]
+
+    if k == 0 or k == n:
+        cache[n][k] = 1
+    else:
+        cache[n][k] = bino(n-1, k-1) + bino(n-1, k)
+
+    return cache[n][k]
+
+print(bino(N, K) % 10007)
