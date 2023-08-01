0bc5bc34494dbc1e3043f98eb93f399506246073
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-01 19:45:06 +0900

    Update 회문 문자열 검사.py

--- 파이썬 알고리즘 문제풀이 입문/섹션 3 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/회문 문자열 검사.py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 3 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/회문 문자열 검사.py
@@ -1,19 +1,31 @@
-N = int(input())
+# N = int(input())
+# a = []
+# for i in range(N):
+#     s = input()
+#     s = s.lower()
+#     t = list(s)
+#     t.reverse()
+#     r = ''.join(t)
 
-a = []
+#     if s == r:
+#         a.append(f'#{i+1} YES')
+#     else:
+#         a.append(f'#{i+1} NO')
+
+# for p in a:
+#     print(p)
+
+#----------------------------------
+
+N = int(input())
 
 for i in range(N):
     s = input()
-    s = s.lower()
-    t = list(s)
-    t.reverse()
-    r = ''.join(t)
-
-    if s == r:
-        a.append(f'#{i+1} YES')
+    s = s.upper()
+    size = len(s)
+    for j in range(size // 2):
+        if s[j] != s[-j-1]:
+            print(f'#{i+1} NO')
+            break
     else:
-        a.append(f'#{i+1} NO')
-
-for p in a:
-    print(p)
-
+        print(f'#{i+1} YES')
