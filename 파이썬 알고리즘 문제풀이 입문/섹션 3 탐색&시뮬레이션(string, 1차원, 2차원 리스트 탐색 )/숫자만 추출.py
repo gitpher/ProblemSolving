8dfb3f146ffaa0c984a40e59c837c543dfb66eee
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-01 20:01:09 +0900

    Update 숫자만 추출.py

--- 파이썬 알고리즘 문제풀이 입문/섹션 3 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/숫자만 추출.py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 3 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/숫자만 추출.py
@@ -1,15 +1,30 @@
+#s = input()
+#
+# a = ''
+# for c in s:
+#     if c.isdigit():
+#         a += c
+#
+# b = int(a)
+# cnt = 0
+# for i in range(1, b+1):
+#     if b % i == 0:
+#         cnt += 1
+#
+# print(b)
+# print(cnt)
+
+
 s = input()
 
-a = ''
-for c in s:
-    if c.isdigit():
-        a += c
+res = 0
+for x in s:
+    if x.isdecimal():
+        res = res * 10 + int(x)
 
-b = int(a)
 cnt = 0
-for i in range(1, b+1):
-    if b % i == 0:
+for i in range(1, res+1):
+    if res % i == 0:
         cnt += 1
 
-print(b)
-print(cnt)
+print(f'{res}\n{cnt}')
