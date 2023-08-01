3230e9949437a6351452a61af0a8b2879945daae
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-01 12:30:14 +0900

    Update 자릿수의 합.py

--- 파이썬 알고리즘 문제풀이 입문/자릿수의 합.py
+++ 파이썬 알고리즘 문제풀이 입문/자릿수의 합.py
@@ -4,9 +4,13 @@ a = list(map(int, input().split()))
 
 def digit_sum(x):
     sum = 0
+    '''
     while x > 0:
-        sum += x % 10
-        x = x // 10
+    sum += x % 10
+    x = x // 10
+    '''
+    for i in str(x):
+        sum += int(i)
     return sum
 
 
