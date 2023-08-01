5afb5003e6fa7d25dc920199456b904d64acd4ae
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-01 20:28:44 +0900

    Create 카드 역배치(정올 기출).py

---
+++ 파이썬 알고리즘 문제풀이 입문/섹션 3 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/카드 역배치(정올 기출).py
@@ -0,0 +1,13 @@
+a = list(range(1, 21))
+
+# s = 5 # idx = 4, -16   (5 - 20 - 1)
+# e = 10 # idx = 9, -11   (10 - 20 - 1)
+
+for j in range(10):
+    s, e = map(int, input().split())
+    l = e - s + 1
+
+    for i in range(l // 2):
+        a[s-1+i], a[e-20-1-i] = a[e-20-1-i], a[s-1+i]
+
+print(*a)
