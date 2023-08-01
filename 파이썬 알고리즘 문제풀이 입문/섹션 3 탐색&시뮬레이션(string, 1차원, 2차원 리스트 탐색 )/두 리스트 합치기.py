1daa493619a5ab9bf8ba99724d43288554abb9b6
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-01 20:51:35 +0900

    Update 두 리스트 합치기.py

--- 파이썬 알고리즘 문제풀이 입문/섹션 3 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/두 리스트 합치기.py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 3 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/두 리스트 합치기.py
@@ -3,7 +3,24 @@ a = list(map(int, input().split()))
 M = int(input())
 b = list(map(int, input().split()))
 
-a.extend(b)
-a.sort()
+# a.extend(b)
+# a.sort()
+#
+# print(*a)
 
-print(*a)
+p1 = p2 = 0
+c = []
+
+while p1<N and p2<M:
+    if a[p1] <= b[p2]:
+        c.append(a[p1])
+        p1 += 1
+    else:
+        c.append(b[p2])
+        p2 += 1
+if p1 < N:
+    c = c + a[p1:]
+if p2 < M:
+    c = c + b[p2:]
+
+print(*c)
