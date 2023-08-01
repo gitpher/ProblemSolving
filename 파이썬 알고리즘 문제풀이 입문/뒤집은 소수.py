7b56ffce3a98a2da4285e5dc2cdbc951bf249b43
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-01 13:30:17 +0900

    Update 뒤집은 소수.py

--- 파이썬 알고리즘 문제풀이 입문/뒤집은 소수.py
+++ 파이썬 알고리즘 문제풀이 입문/뒤집은 소수.py
@@ -1,10 +1,18 @@
 N = int(input())
 a = list(map(int, input().split()))
 
-def reverse(x):
-    a = list(str(x))
-    a.reverse()
-    return int(''.join(a))
+# def reverse(x):
+#     a = list(str(x))
+#     a.reverse()
+#     return int(''.join(a))
+
+def reverse(x): # 5050
+    res = 0
+    while x > 0:
+        t = x % 10
+        x = x // 10
+        res = res * 10 + t
+    return res
 
 def isPrime(x):
     for i in range(2, x):
