f61069d7ae4d62347a08164faed3688438efccee
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-01 21:29:15 +0900

    Create 수들의 합.py

---
+++ 파이썬 알고리즘 문제풀이 입문/섹션 3 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/수들의 합.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())
+a = list(map(int, input().split()))
+
+lt = 0
+rt = 1
+tot = a[0]
+cnt = 0 # ans
+
+while True:
+    if tot < M:
+        if rt < N:
+            tot += a[rt]
+            rt += 1
+        else:
+            break
+    elif tot == M:
+        cnt += 1
+        tot -= a[lt]
+        lt += 1
+    else:
+        tot -= a[lt]
+        lt += 1
+
+
+print(cnt)
