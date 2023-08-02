0ad6b57f2f6bce29c15710c45acade4e24d45c83
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-02 20:38:43 +0900

    Create 침몰하는 타이타닉(그리디).py

---
+++ 파이썬 알고리즘 문제풀이 입문/섹션 4 이분탐색(결정알고리즘) & 그리디 알고리즘/침몰하는 타이타닉(그리디).py
@@ -0,0 +1,38 @@
+import pickle
+
+N, M = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+
+# 실패한 방법
+# cnt = 1
+# tot = 0
+# for w in a:
+#     if tot + w > M:
+#         cnt += 1
+#         tot = w
+#     else:
+#         tot += w
+
+# 내 방식으로 성공한 방법
+# lt = 0
+# rt = N - 1
+# cnt = 0
+# while lt <= rt:
+#     if a[lt] + a[rt] <= M:
+#         lt += 1
+#         rt -= 1
+#     else:
+#         rt -= 1
+#     cnt += 1
+
+cnt = 0
+while a:
+    if a[0] + a[-1] > M:
+        a.pop()
+    else:
+        a.pop(0)
+        a.pop()
+    cnt += 1
+
+print(cnt)
