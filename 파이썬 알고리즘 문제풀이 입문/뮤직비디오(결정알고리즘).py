116066f9645893b2b97603be88c703f87b1ed69f
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-02 16:23:24 +0900

    Create 뮤직비디오(결정알고리즘).py

---
+++ 파이썬 알고리즘 문제풀이 입문/뮤직비디오(결정알고리즘).py
@@ -0,0 +1,30 @@
+N, M = map(int, input().split())
+a = list(map(int, input().split()))
+
+# 최소
+# 용량
+
+lt = max(a) # 최소 가장 큰 길이의 곡을 담을 수 있어야 함
+rt = sum(a) # 모든 곡의 합을 M개의 CD가 담을 수 있어야 함
+
+#용량
+def fun(mid):
+    tot = 0
+    cnt = 1
+    for l in a:
+        if tot + l > mid:
+            tot = l
+            cnt += 1
+        else:
+            tot += l
+    return cnt
+
+#최소
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if fun(mid) <= M:
+        rt = mid - 1
+    else:
+        lt = mid + 1
+
+print(lt)
