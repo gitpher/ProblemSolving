c99e2663d600ab4c4cd41b74aebbf7bef69340ae
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-02 20:18:37 +0900

    Update 창고 정리.py

--- 파이썬 알고리즘 문제풀이 입문/섹션 4 이분탐색(결정알고리즘) & 그리디 알고리즘/창고 정리.py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 4 이분탐색(결정알고리즘) & 그리디 알고리즘/창고 정리.py
@@ -2,15 +2,20 @@ L = int(input())
 a = list(map(int, input().split()))
 M = int(input())
 
+# for m in range(M):
+#     min_val = min(a)
+#     min_idx = a.index(min_val)
+#     max_val = max(a)
+#     max_idx = a.index(max_val)
+#
+#     a[min_idx] += 1
+#     a[max_idx] -= 1
+#
+# print(max(a) - min(a))
 
-for m in range(M):
-    min_val = min(a)
-    min_idx = a.index(min_val)
-    max_val = max(a)
-    max_idx = a.index(max_val)
-
-    a[min_idx] += 1
-    a[max_idx] -= 1
-
-print(max(a) - min(a))
-
+a.sort()
+for _ in range(M):
+    a[0] += 1
+    a[-1] -= 1
+    a.sort()
+print(a[-1] - a[0])
