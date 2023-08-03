59a808715a91ab5451254369579bc04ae39a9daf
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-04 01:40:07 +0900

    Update 교육과정설계.py

--- 파이썬 알고리즘 문제풀이 입문/섹션 5 자료구조 활용 (스택, 큐, 해쉬, 힙)/교육과정설계.py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 5 자료구조 활용 (스택, 큐, 해쉬, 힙)/교육과정설계.py
@@ -5,11 +5,14 @@ N = int(input())
 
 for i in range(N):
     dq = deque(S)
-    for j in input():
-        if j in dq:
-            dq.popleft()
-    if len(dq) == 0:
-        print(f'#{i+1} YES')
+    for ch in input():
+        if ch in dq:
+            if ch != dq.popleft():
+                print(f'#{i + 1} NO')
+                break
     else:
-        print(f'#{i + 1} NO')
-
+        if len(dq) == 0:
+            print(f'#{i + 1} YES')
+        else: # 순서대로 필수교육과정이 있었지만 다 있지는 않았을 수 있음
+            print(f'#{i + 1} NO')
+            
