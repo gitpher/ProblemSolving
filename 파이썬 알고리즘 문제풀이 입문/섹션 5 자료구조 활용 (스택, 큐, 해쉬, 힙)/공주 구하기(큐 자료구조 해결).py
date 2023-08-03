6b264024f1f9c91b1501dc2843215702a7375c8d
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-04 00:30:23 +0900

    Update 공주 구하기(큐 자료구조 해결).py

--- 파이썬 알고리즘 문제풀이 입문/섹션 5 자료구조 활용 (스택, 큐, 해쉬, 힙)/공주 구하기(큐 자료구조 해결).py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 5 자료구조 활용 (스택, 큐, 해쉬, 힙)/공주 구하기(큐 자료구조 해결).py
@@ -8,4 +8,22 @@ for i in range(N - 1):
         a.append(a.popleft())
     a.popleft()
 
+from collections import deque
+N, K = map(int, input().split())
+a = list(range(1, N + 1))
+a = deque(a)
+
+for i in range(N - 1):
+    for j in range(K - 1):
+        a.append(a.popleft())
+    a.popleft()
+
 print(a[0])
+
+# while a:
+#     for _ in range(K - 1):
+#         a.append(a.popleft())
+#     a.popleft()
+#     if len(a) == 1:
+#         print(a[0])
+
