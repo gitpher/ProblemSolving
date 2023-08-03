584ec717a7eed3103603a067bdc63be826ac7198
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-04 02:10:41 +0900

    Update 단어 찾기.py

--- 파이썬 알고리즘 문제풀이 입문/섹션 5 자료구조 활용 (스택, 큐, 해쉬, 힙)/단어 찾기.py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 5 자료구조 활용 (스택, 큐, 해쉬, 힙)/단어 찾기.py
@@ -1,14 +1,23 @@
-from collections import deque
-
 N = int(input())
-a = []
-b = []
+# a = []
+# b = []
+#
+# for _ in range(N):
+#     a.append(input())
+#
+# for _ in range(N - 1):
+#     b.append(input())
+#
+# c = set(a) - set(b)
+# print(c.pop())
 
-for _ in range(N):
-    a.append(input())
+a = dict()
+for i in range(N):
+    a[input()] = 1
 
-for _ in range(N - 1):
-    b.append(input())
+for j in range(N - 1):
+    a[input()] = 0
 
-c = set(a) - set(b)
-print(c.pop())
+for key, val in a.items():
+    if val == 1:
+        print(key)
