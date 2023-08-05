00ab77dbefa9c7e71c2db3c131c50ca18d3f7107
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-06 01:33:22 +0900

    Update 바둑이 승차(DFS).py

--- 파이썬 알고리즘 문제풀이 입문/섹션 6 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/바둑이 승차(DFS).py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 6 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/바둑이 승차(DFS).py
@@ -19,19 +19,22 @@ for i in range(N):
 # dfs(0, 0)
 # print(max(b))
 
-
+tot = sum(a)
 res = float('-inf')
-def dfs(lvl, sum):
+def dfs(lvl, sum, tsum):
+    global tot
     global res
 
+    if sum + (tot - tsum) < res: # 지금까지의 선택과 나머지 선택의 합이 지금 가진 답보다 작다면 더 볼 필요 없음
+        return
     if sum > C:
         return
     if lvl == N:
         if sum > res:
             res = sum
     else:
-        dfs(lvl + 1, sum + a[lvl])
-        dfs(lvl + 1, sum)
+        dfs(lvl + 1, sum + a[lvl], tsum + a[lvl])
+        dfs(lvl + 1, sum, tsum + a[lvl])
 
-dfs(0, 0)
+dfs(0, 0, 0)
 print(res)
