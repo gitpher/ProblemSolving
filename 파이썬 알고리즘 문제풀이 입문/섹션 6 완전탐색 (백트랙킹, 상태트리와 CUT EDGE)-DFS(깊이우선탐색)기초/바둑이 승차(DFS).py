58bfe9f673307cf300946aac49396ba1c89296db
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-06 01:12:36 +0900

    Update 바둑이 승차(DFS).py

--- 파이썬 알고리즘 문제풀이 입문/섹션 6 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/바둑이 승차(DFS).py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 6 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/바둑이 승차(DFS).py
@@ -4,20 +4,34 @@ a = []
 for i in range(N):
     a.append(int(input()))
 
-b = []
+# b = []
+# def dfs(lvl, sum):
+#     if lvl == N:
+#         b.append(sum)
+#         return
+#     else:
+#         if sum + a[lvl] > C:
+#             b.append(sum)
+#             return
+#         else:
+#             dfs(lvl + 1, sum + a[lvl]) # 경우 1
+#             dfs(lvl + 1, sum) # 경우 2
+# dfs(0, 0)
+# print(max(b))
 
+
+res = float('-inf')
 def dfs(lvl, sum):
+    global res
 
-    if lvl == N:
-        b.append(sum)
+    if sum > C:
         return
+    if lvl == N:
+        if sum > res:
+            res = sum
     else:
-        if sum + a[lvl] > C:
-            b.append(sum)
-            return
-        else:
-            dfs(lvl + 1, sum + a[lvl])
-            dfs(lvl + 1, sum)
+        dfs(lvl + 1, sum + a[lvl])
+        dfs(lvl + 1, sum)
 
 dfs(0, 0)
-print(max(b))
+print(res)
