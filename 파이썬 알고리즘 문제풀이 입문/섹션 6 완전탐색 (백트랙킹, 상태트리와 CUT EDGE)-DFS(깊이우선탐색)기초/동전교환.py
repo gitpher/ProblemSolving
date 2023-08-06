f9d6b13de7bb3b294c4302b967840a1f3653ac40
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-06 13:06:48 +0900

    Update 동전교환.py

--- 파이썬 알고리즘 문제풀이 입문/섹션 6 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/동전교환.py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 6 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/동전교환.py
@@ -2,19 +2,32 @@ N = int(input())
 a = list(map(int, input().split()))
 M = int(input())
 
+a.sort(reverse=True) # 가장 적은 수의 동전 갯수를 구해야 하기 떄문에 큰 단위부터
+
 res = float('inf')
+# def dfs(lvl, sum):
+#     global res
+#     if sum == M:
+#         if res > lvl:
+#             res = lvl
+#             return
+#     else:
+#         for i in range(N):
+#             if sum + a[i] > M:
+#                 return
+#             else:
+#                 dfs(lvl + 1, sum + a[i])
 def dfs(lvl, sum):
     global res
+    if sum > M:
+        return
+
     if sum == M:
         if res > lvl:
             res = lvl
-            return
     else:
         for i in range(N):
-            if sum + a[i] > M:
-                return
-            else:
-                dfs(lvl + 1, sum + a[i])
+            dfs(lvl + 1, sum + a[i])
 
 
 dfs(0, 0)
