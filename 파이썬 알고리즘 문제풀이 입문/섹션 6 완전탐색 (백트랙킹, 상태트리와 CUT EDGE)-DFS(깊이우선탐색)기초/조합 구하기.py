f3490589f8fd76ca866aa3d2f804545b0c0aa3e7
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-07 18:09:57 +0900

    Update 조합 구하기.py

--- 파이썬 알고리즘 문제풀이 입문/섹션 6 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/조합 구하기.py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 6 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/조합 구하기.py
@@ -1,22 +1,17 @@
 N, M = map(int, input().split())
 
 cmb = [0] * M
-chk = [0] * (N + 1)
-
 
 cnt = 0
-def dfs(lvl, r):
+def dfs(lvl, s):
     global cnt
     if lvl == M:
         cnt += 1
         print(*cmb)
     else:
-        for i in range(r, N + 1):
-            if chk[i] == 0:
-                chk[i] = 1
-                cmb[lvl] = i
-                dfs(lvl + 1, r + i)
-                chk[i] = 0
+        for i in range(s, N + 1):
+            cmb[lvl] = i
+            dfs(lvl + 1, i + 1)
 
 
 dfs(0 ,1)
