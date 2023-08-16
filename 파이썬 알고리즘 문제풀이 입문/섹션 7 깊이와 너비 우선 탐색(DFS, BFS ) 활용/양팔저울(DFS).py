12b07b9e984b279c8a45534f74f83982c04d7110
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-16 22:36:47 +0900

    Update 양팔저울(DFS).py

--- 파이썬 알고리즘 문제풀이 입문/섹션 7 깊이와 너비 우선 탐색(DFS, BFS ) 활용/양팔저울(DFS).py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 7 깊이와 너비 우선 탐색(DFS, BFS ) 활용/양팔저울(DFS).py
@@ -1,17 +1,31 @@
 K = int(input())
 a = list(map(int, input().split()))
 tot = sum(a)
-res = set(range(1, max(a)))
 
+# res = set(range(1, max(a)))
+# def dfs(lvl, sum):
+#     if lvl == K:
+#         res.add(sum)
+#     else:
+#         dfs(lvl + 1, sum + a[lvl])
+#         dfs(lvl + 1, sum)
+# dfs(0, 0)
+# print(tot - len(res))
 
+res = set()
 def dfs(lvl, sum):
+    global res
     if lvl == K:
-        res.add(sum)
+        if 0 < sum <= tot:
+            res.add(sum)
     else:
         dfs(lvl + 1, sum + a[lvl])
+        dfs(lvl + 1, sum - a[lvl])
         dfs(lvl + 1, sum)
 
-
-dfs(0, 0)
-
+dfs(0,0)
 print(tot - len(res))
+
+
+
+
