b8fefbe6a967f999e700da3b243a7bf9eca23f3b
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-08-26 00:24:35 +0900

    Update 같은 숫자는 싫어.py

--- Programmers/같은 숫자는 싫어.py
+++ Programmers/같은 숫자는 싫어.py
@@ -1,10 +1,20 @@
+# def solution(arr):
+#     answer = []
+    
+#     chk = [10]
+#     for x in arr:
+#         if x != chk[0]:
+#             answer.append(x)
+#             chk[0] = x
+    
+#     return answer
+
 def solution(arr):
     answer = []
+    answer.append(arr[0])
     
-    chk = [10]
-    for x in arr:
-        if x != chk[0]:
-            answer.append(x)
-            chk[0] = x
-    
+    for i in range(1, len(arr)):
+        if arr[i] != arr[i-1]:
+            answer.append(arr[i])
+            
     return answer
