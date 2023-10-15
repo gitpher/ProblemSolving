03f714145df399f5c1fab70fc0154b6d74a60fba
Junwoo Choi <79530534+gitpher@users.noreply.github.com>
2023-10-15 18:05:32 +0900

    Update KCPC (백준 3758번).py

--- Baekjoon/KCPC (백준 3758번).py
+++ Baekjoon/KCPC (백준 3758번).py
@@ -15,8 +15,8 @@ for _ in range(T):
             c[team_id] = [[0] * (question_len + 1), 0, 1, i] # question_score_list, score_val, submit_cnt, submit_time
             c[team_id][0][question_num] += score_val
 
-        for v in c.values():
-            v[1] = sum(v[0])
+    for v in c.values():
+        v[1] = sum(v[0])
 
     for idx, val in enumerate(sorted(c.items(), key=lambda x: (-x[1][1], x[1][2], x[1][3]))):
         if val[0] == my_team_id:
